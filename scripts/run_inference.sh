41c84b2553a12829117c2cd394f59b843fbf71b2
Alexis Fox <alexis.fox@duke.edu>
2025-05-29 12:43:11 -0400

    test commit

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -3,6 +3,6 @@
 python scripts/run_inference.py \
   --model_path model/checkpoints/alibi_model.pt \
   --interactive
-  --max_new_tokens 20 \
+  --max_new_tokens 25 \
   --temperature 0.8 \
   --top_k 40
