4dac388969e9fff4b6dbcfffdec4738028a5a0bc
Alexis Fox <alexis.fox@duke.edu>
2025-06-02 13:14:01 -0400

    tsr

--- scripts/run_lens.sh
+++ scripts/run_lens.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python examples/train_lens.py \ 
-  --model_ckpt model/checkpoints/alibi_model.pt \
-  --dataset "wikimedia/wikipedia" \
-  --dataset_config "20231101.en" \
-  --output_dir ./outputs/tuned_lens_heads \
-  --epochs 5
+python ./examples/train_lens.py \ 
+    --model_ckpt "model/checkpoints/alibi_model.pt" \
+    --dataset "wikimedia/wikipedia" \
+    --dataset_config "20231101.en" \
+    --output_dir ./outputs/tuned_lens_heads \
+    -epochs 5
