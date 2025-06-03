89c222056cfe4a74a0d54ba2f24095eb12702601
Alexis Fox <alexis.fox@duke.edu>
2025-06-03 16:39:28 -0400

    remove fluff

--- experiments/run_checkpoints.py
+++
@@ -1,3 +0,0 @@
-from model.model_token_factored_alibi import FactoredTransformerModelALiBi as ModelALiBi
-
-model, tokenizer = ModelALiBi.load_from_checkpoint('output_alibi/alibi_model.pt',device='cuda')
